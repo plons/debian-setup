67602d7e2ab3506bd3acd00e0d75ac8eeb768bd0
Peter Lodewyckx <plodewyckx@key.net>
2014-11-25 23:14:09 +0100

    Added vim-visual-star-search

--- install.sh
+++ install.sh
@@ -12,12 +12,13 @@ assertFilePresent $DROPBOX_SCRIPTS_DIR/bashrc
 assertFilePresent $DROPBOX_SCRIPTS_DIR/vimrc
 assertFilePresent $DROPBOX_SCRIPTS_DIR/ssh_config
 
-updateOrInstall vim-pathogen      https://github.com/tpope/vim-pathogen.git
-updateOrInstall vim-nerdtree      https://github.com/scrooloose/nerdtree.git
-updateOrInstall vim-nerdcommenter https://github.com/scrooloose/nerdcommenter.git
-updateOrInstall vim-fugitive      https://github.com/tpope/vim-fugitive.git
+updateOrInstall vim-pathogen           https://github.com/tpope/vim-pathogen.git
+updateOrInstall vim-nerdtree           https://github.com/scrooloose/nerdtree.git
+updateOrInstall vim-nerdcommenter      https://github.com/scrooloose/nerdcommenter.git
+updateOrInstall vim-fugitive           https://github.com/tpope/vim-fugitive.git
+updateOrInstall vim-visual-star-search https://github.com/bronson/vim-visual-star-search.git
 #updateOrInstall vim-taglist       https://github.com/vim-scripts/taglist.vim.git
-updateOrInstall vim-taglist       http://sourceforge.net/projects/vim-taglist/files/latest/download?source=files 
+#updateOrInstall vim-taglist       http://sourceforge.net/projects/vim-taglist/files/latest/download?source=files 
 
 verifySymlink $DROPBOX_SCRIPTS_DIR/bashrc         $HOME_DIR/.bashrc
 verifySymlink $DROPBOX_SCRIPTS_DIR/bashrc.d       $HOME_DIR/.bashrc.d
