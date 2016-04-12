9080539a25fe4826b8398bd513d02898b4e82551
Peter Lodewyckx <plodewyckx@key.net>
2016-04-12 09:56:15 +0200

    install tmux.conf + select ssh config

--- install.sh
+++ install.sh
@@ -33,11 +33,58 @@ source helper-functions.sh
 ################################################################################
 # Verify presence of expected directories and files
 ################################################################################
+actions=()
 assertDirectoryPresent $HOME_DIR
 assertDirectoryPresent $DROPBOX_SCRIPTS_DIR/bashrc.d
 assertFilePresent $DROPBOX_SCRIPTS_DIR/bashrc
-assertFilePresent $DROPBOX_SCRIPTS_DIR/ssh/external.config
+assertFilePresent $DROPBOX_SCRIPTS_DIR/ssh/config.external
+assertFilePresent $DROPBOX_SCRIPTS_DIR/ssh/config.internal
+assertFilePresent $DROPBOX_SCRIPTS_DIR/ssh/config.sshuttle
 
 verifySymlink $DROPBOX_SCRIPTS_DIR/bashrc              $HOME_DIR/.bashrc
 verifySymlink $DROPBOX_SCRIPTS_DIR/bashrc.d            $HOME_DIR/.bashrc.d
-verifySymlink $DROPBOX_SCRIPTS_DIR/ssh/external.config $HOME_DIR/.ssh/config
+verifySymlink $DROPBOX_SCRIPTS_DIR/tmux.conf           $HOME_DIR/.tmux.conf
+actions+=("Verified symbolic link to bashrc file")
+actions+=("Verified symbolic link to tmux config")
+
+cmd=(dialog --separate-output --checklist "Select ssh config file:" 22 76 16)
+options=(
+   1 "internal" on    # any option can be set to default to "on"
+   2 "external" off
+   3 "sshuttle" off)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+   case $choice in
+      1) connection="internal";;
+      2) connection="external";;
+      3) connection="sshuttle";;
+      *) connection="internal"; echo "Unknown option!!";;
+   esac
+done
+verifySymlink $DROPBOX_SCRIPTS_DIR/ssh/config.$connection $HOME_DIR/.ssh/config
+actions+=("Selected ssh config $connection")
+
+################################################################################
+# Show info on actions
+################################################################################
+if [ ${#actions[@]} -gt 0 ]; then
+   message="";
+
+   # Add underlined title
+   title="Executed ${#actions[@]} actions:";
+   message="$message$title\n";
+   for ((c=1; c<=${#title}; c++)); do message="$message="; done
+   message="$message\n";
+
+   # Add messages about executed actions
+   for executedAction in "${actions[@]}"; do
+      message="$message  - $executedAction\n";
+   done
+   dialog --title "Info on actions" --msgbox "$message" 1000 1000
+   clear;
+else
+   dialog --title "Info on actions" --msgbox "No actions executed" 12 55
+   clear;
+fi
